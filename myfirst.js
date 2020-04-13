add03f54c150d45b9edeb66115ebc1cb98c338d8
martinchez <martinchege1998@gmail.com>
2020-04-13 19:02:16 +0300

    first commit

--- myfirst.js
+++ myfirst.js
@@ -3,6 +3,6 @@ var dt =require('./myfirstmodule')
 
 http.createServer(function (req,res) {
     res.writeHead(200,{'content-Type':'text/html'});
-    res.write("the date and time are curently:" + dt.myDateTime());
+    res.write("the date and time are cur vb;m,.?ently:" + dt.myDateTime());
     res.end("welcome again");
 }).listen(8080);
